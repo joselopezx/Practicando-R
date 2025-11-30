15bb091c7198d5d9ccfc3ca5be682cefc7e6470e
joselopezx <jose0xlopez@gmail.com>
2025-11-29 22:43:15 -0600

    Add files via upload

---
+++ Bucles for y while/Comprendiendo lazos.R
@@ -0,0 +1,11 @@
+#Ana está desarrollando un programa que necesita procesar una lista de 5 nombres
+#de clientes para generar informes mensuales. Para ello, 
+#necesita escribir un programa
+#que recorra la lista de nombres y muestre cada cliente.
+
+
+clientes <- list("Juan", "Maria", "Carlos", "Ana", "Beatriz")
+
+for(cliente in clientes){
+  print(paste("Cliente : ",cliente))
+}
